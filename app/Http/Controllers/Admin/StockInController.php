2b3b031d118cd5961190dc02acf62dd205f940e0
Insath97 <insath1997.mi@gmail.com>
2024-11-24 18:00:09 +0530

    get office data with warehouse data

--- app/Http/Controllers/Admin/StockInController.php
+++ app/Http/Controllers/Admin/StockInController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\StockTransaction;
 use Illuminate\Http\Request;
 
 class StockInController extends Controller
@@ -11,13 +12,28 @@ class StockInController extends Controller
 
     public function create() {}
 
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+        $stock_in = new StockTransaction();
+        $stock_in->product_id = $request->product_id;
+        $stock_in->warehouse_id = $request->warehouse_id;
+        $stock_in->transaction_type = 'in';
+        $stock_in->quantity = $request->quantity;
+        $stock_in->remarks = $request->remarks;
+        $stock_in->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Stock added successfully',
+            'data' => $stock_in,
+        ], 201);
+    }
 
     public function show(string $id) {}
 
     public function edit(string $id) {}
 
     public function update(Request $request, string $id) {}
-    
+
     public function destroy(string $id) {}
 }
